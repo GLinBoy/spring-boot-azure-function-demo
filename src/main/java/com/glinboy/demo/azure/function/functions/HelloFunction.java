b79cc1b51f988430254d886ebbc72e5582f11f52
GLinBoy <glinboy@live.com>
2024-03-14 17:48:22 +0100

    📦 NEW: Added a Hello function
    resolved #11

---
+++ src/main/java/com/glinboy/demo/azure/function/functions/HelloFunction.java
@@ -0,0 +1,34 @@
+package com.glinboy.demo.azure.function.functions;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Service;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+@Service
+public class HelloFunction {
+
+	@FunctionName("hello")
+	public HttpResponseMessage execute(@HttpTrigger(name = "request", methods = { HttpMethod.GET,
+			HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+			ExecutionContext context) {
+		
+		String name = request.getQueryParameters().getOrDefault("name", "world");
+		
+		return request
+				.createResponseBuilder(HttpStatus.OK)
+				.body(Map.of("message", name))
+				.header("Content-Type", "application/json")
+				.build();
+	}
+}
