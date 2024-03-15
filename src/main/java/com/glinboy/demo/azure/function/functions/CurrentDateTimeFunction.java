c033ef1da7a6d8c6602dbf823f716430466c7261
GLinBoy <glinboy@live.com>
2024-03-15 20:27:15 +0100

    Enhanced function to use DTO and java function

--- src/main/java/com/glinboy/demo/azure/function/functions/CurrentDateTimeFunction.java
+++ src/main/java/com/glinboy/demo/azure/function/functions/CurrentDateTimeFunction.java
@@ -1,15 +1,16 @@
 package com.glinboy.demo.azure.function.functions;
 
-import java.time.Instant;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Service;
 
+import com.glinboy.demo.azure.function.dto.ZoneDTO;
+import com.glinboy.demo.azure.function.service.FormatDateTime;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -19,27 +20,31 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CurrentDateTimeFunction {
 	
-	@Value("${application.config.date-time-pattern}")
-	private String dateTimePattern;
+	private final FormatDateTime formatDateTime;
 	
 	@FunctionName("now")
 	public HttpResponseMessage execute(
 			@HttpTrigger(name = "request",
 				methods = { HttpMethod.GET, HttpMethod.POST },
-				authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+				authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<ZoneDTO>> request,
 			ExecutionContext context) {
-		Instant now = Instant.now();
-		String nowUTC = DateTimeFormatter
-			.ofPattern(dateTimePattern)
-			.withZone(ZoneOffset.UTC)
-			.format(now);
+		Set<ZoneOffset> zones = new HashSet<>();
+		zones.add(ZoneOffset.UTC);
+		request.getBody().filter(z -> z.zone() != null)
+			.ifPresent(z -> zones.add(z.zone()));
+		
+		Map<String, String> response = zones.stream()
+			.collect(Collectors.toMap(Object::toString, formatDateTime::apply));
 		
 		return request
 				.createResponseBuilder(HttpStatus.OK)
-				.body(Map.of("UTC", String.format("%s", nowUTC)))
+				.body(response)
 				.header("Content-Type", "application/json")
 				.build();
 	}
